b3184f6bec132d41517f94858b423f03bf091d70
Ali-Ghuman <69914918+Ali-Ghuman@users.noreply.github.com>
2020-08-23 20:12:12 -0400

    Update currency.cpp

--- C++/currency.cpp
+++ C++/currency.cpp
@@ -4,11 +4,11 @@ int main()
 {
   double pesos, reais, soles, dollars; 
 
-  std::cout << "Enter number of Colombian Pesos: "; // 0.045
+  std::cout << "Enter number of Colombian Pesos: "; 
   std::cin >> pesos; 
-  std::cout << "Enter number of Brazilian Reais: "; // 0.18
+  std::cout << "Enter number of Brazilian Reais: "; 
   std::cin >> reais; 
-  std::cout << "Enter number of Peruvian Soles: "; // 0.28 
+  std::cout << "Enter number of Peruvian Soles: ";  
   std::cin >> soles; 
   
   dollars = (.045 * pesos) + (.18 * reais) + (.28 * soles); 
