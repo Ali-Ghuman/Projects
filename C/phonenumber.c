7c137627683e1f1fe93e9a7e1a320486687d4a26
Ali Ghuman <alighuman50@gmail.com>
2020-08-24 17:02:37 -0400

    Add lowercase functionality

--- C/phonenumber.c
+++ C/phonenumber.c
@@ -1,5 +1,6 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+#include <ctype.h>
 
 
 int main() {
@@ -8,7 +9,7 @@ int main() {
     printf("Enter a phone number: ");
 
    do {
-       ch = getchar();
+       ch = toupper(getchar());
        switch (ch) 
        {
            case 'A': 
