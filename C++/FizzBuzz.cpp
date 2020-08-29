d1903e05e5e56388fef9a5c0b2e0b3e514857232
Ali Ghuman <alighuman50@gmail.com>
2020-08-29 16:21:21 -0400

    Refine

--- C++/FizzBuzz.cpp
+++ C++/FizzBuzz.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
-
+#define fizz 3
+#define buzz 5
+#define fizzbuzz 15
 
 int main () 
 {
 
   for (int i = 1; i <= 100; i++)  // if divisible by 3 output Fizz, if divisible by 5 output Buzz, if divisible by both 3 & 5 output FizzBuzz. 
   {
-    if ( i % 3 == 0 && i % 5 == 0) 
+    if (!(i % fizzbuzz)) 
     {
       std::cout << "FizzBuzz\n"; 
     }
-    else if ( i % 3 == 0) 
+    else if (!(i % fizz)) 
     {
       std::cout << "Fizz\n"; 
     }
-    else if ( i % 5 == 0) 
+    else if (!(i % buzz)) 
     {
       std::cout << "Buzz\n"; 
     } 
     else 
     {
       std::cout << i << "\n";
-    }
+    } 
   }
 
   return 0; 
